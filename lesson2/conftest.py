d58087b11245d8422b2567571505712320eefab2
Tigran <tigra.azaryan@me.com>
2024-04-07 07:18:31 -0700

    Add browser options and page load strategy

--- lesson2/conftest.py
+++ lesson2/conftest.py
@@ -1,7 +1,7 @@
-import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+import pytest
 from data import *
 from locators import *
 
@@ -9,6 +9,12 @@ from locators import *
 @pytest.fixture(scope="function", autouse=True)
 def browser():
     chrome_options = webdriver.ChromeOptions()
+    # chrome_options.page_load_strategy = "eager"
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--incognito")
+    chrome_options.add_argument("--ignore-certificate-errors")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--disable-cache")
 
     service = Service(ChromeDriverManager().install())
     browser = webdriver.Chrome(options=chrome_options, service=service)
